8ae97f165f1a35ef29ac7b59a5798cc39882f324
JuanABG <juanabg29108@hotmail.com>
2023-04-18 14:38:19 -0500

    Servicios

--- src/app/Services/api.service.ts
+++ src/app/Services/api.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,37 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
-  constructor() { }
-}
+  constructor( public http: HttpClient) {
+  }
+   url= "https://localhost:7292/";
+
+  public async  get(controlador:String){
+
+   var response:any
+   await this.http.get(this.url+controlador).subscribe(res=>{
+     response=res
+   });
+    return response
+    }
+    public async  post(controlador:String,data:object){
+    var response:any
+    await this.http.post(this.url+controlador, data).subscribe(res=>{
+    response=res
+    });
+    return response
+    }
+    public async  put(controlador:String,data:object,id:number){
+      var response:any
+      await this.http.put(this.url+controlador+'/'+id, data).subscribe(res=>{
+      response=res
+    });
+      return response
+    }
+    public async  delete(controlador:String, id:number){
+      var response:any
+      await this.http.delete(this.url+controlador+'/'+id).subscribe(res=>{
+      response=res
+    });
+      return response
+    }
+ }
